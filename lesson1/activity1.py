d3455565031ae68d4b0a2e5d01775a44db131a02
codernaa29 <01717360938hawa@gmail.com>
2025-08-08 13:12:31 +0600

    Lesson 1 done

---
+++ lesson1/activity1.py
@@ -0,0 +1,19 @@
+print("Welcome to the Python Class")
+print("\n")
+print("Student Profile")
+# Display name
+print("Name : Noshin Alam")
+# Display grade
+print("Grade : 9")
+# Display group
+print("Group : Science")
+# Display school
+print("School : Barishal Govt. Model School & College")
+# Display hobbies
+print("Hobbies : Painting, Singing, Trying out different instrumants.")
+# Display extra curricular activities 
+print("Extra Curricular Activities: Singing, Painting, Coding, Scouting")
+# Display aim
+print("Aim : To become a genetic engineer")
+# Display favourite subjects
+print("Favourite Subjects : Math, Biology, Chemistry")
